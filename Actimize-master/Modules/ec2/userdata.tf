8217b88082921ce96962db0183e333fcb42761cb
JyotikaW <JyotikaW@users.noreply.github.com>
2020-07-22 17:17:56 +0530

    Update userdata.tf

--- Actimize-master/Modules/ec2/userdata.tf
+++ Actimize-master/Modules/ec2/userdata.tf
@@ -10,8 +10,7 @@ net localgroup administrators ${var.INSTANCE_USERNAME} /add
 echo ${base64encode(file("./Actimize-master/Modules/ec2/install-script-npp.ps1"))} > tmp1.b64 && certutil -decode tmp1.b64 install-script-npp.ps1
 echo ${base64encode(file("./Actimize-master/Modules/ec2/install-script-Java.ps1"))} > tmp1.b64 && certutil -decode tmp1.b64 install-script-Java.ps1
 powershell.exe -file "C:\setup-scripts\install-script-Java.ps1"  
-powershell.exe -file "C:\setup-scripts\install-script-npp.ps1"    
-If "${var.customer}"=="nbs" (powershell.exe -file "C:\setup-scripts\install-script-npp.ps1")     
+powershell.exe -file "C:\setup-scripts\install-script-npp.ps1"        
 echo "" > _INIT_COMPLETE
 </script>
 <persist>false</persist>
